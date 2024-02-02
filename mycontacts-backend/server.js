5510f95e5dd8587d70b98ff82f1bdadf96cee7d4
Robin Raj <Robin.Raj@walmart.com>
2024-02-02 23:12:30 +0530

    first commit

---
+++ mycontacts-backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+const port = process.env.port || 5000
+
+
+app.get('/api/contacts', (req, res) => {
+    res.json({"message": "This is a response to the website link which you just gave"})
+})
+
+app.listen(5000, console.log("Server running on port: " + port))
